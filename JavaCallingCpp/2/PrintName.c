851b67089589872f52388047c14389e110b25631
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2021-07-24 23:40:33 +0530

    more c functions are exposed via jni

--- JavaCallingCpp/2/PrintName.c
+++ JavaCallingCpp/2/PrintName.c
@@ -5,7 +5,7 @@ JNIEXPORT void JNICALL Java_PrintName_printName
   (JNIEnv *env, jclass this, jstring name){
 
   const char * myName = (*env)->GetStringUTFChars(env, name, 0);
-  printf("My Name is :- %s.\n", myName);
+  printf("[C++]My Name is :- %s.\n", myName);
   (*env)->ReleaseStringUTFChars(env, name, myName);
     
 }
@@ -13,6 +13,45 @@ JNIEXPORT void JNICALL Java_PrintName_printName
 JNIEXPORT void JNICALL Java_PrintName_greeting
   (JNIEnv *env, jclass this){
 
-  printf("Greeting from C world.\n");
+  printf("[C++]Greeting from C world.\n");
     
 }
+
+JNIEXPORT jint JNICALL Java_PrintName_add
+  (JNIEnv *env, jclass this, jint num1, jint num2){
+
+  printf("[C++]Adding %d and %d.\n", num1, num2);
+  return num1+num2;
+    
+}
+
+// According to link: https://stackoverflow.com/questions/13796786/jstring-return-in-jni-program
+// @nn0p When using env functions in c, the statement is (*env)->Function(env, parameters). However in c++, this turns to: env->Function(parameters). – agastalver Nov 7 '16 at 13:02
+
+JNIEXPORT jstring JNICALL Java_PrintName_append
+  (JNIEnv *env, jclass this, jstring str1, jstring str2){
+
+  const char * myStr1 = (*env)->GetStringUTFChars(env, str1, NULL);
+  printf("[C++]String1 is :- %s.\n", myStr1);
+
+  const char * myStr2 = (*env)->GetStringUTFChars(env, str2, 0);
+  printf("[C++]String2 is :- %s.\n", myStr2);
+
+  // jstring retVal = str1 + str2;
+  jstring retVal;
+
+  char msg[600] = "[C++]COMBINED MSG :";
+
+  strcat(msg, myStr1);
+  strcat(msg, myStr2);
+
+  puts(msg);
+
+  retVal = (*env)->NewStringUTF(env,msg);
+
+  (*env)->ReleaseStringUTFChars(env, str1, myStr1);
+  (*env)->ReleaseStringUTFChars(env, str2, myStr2);
+
+  return retVal;
+
+  }
--- JavaCallingCpp/2/PrintName.java
+++ JavaCallingCpp/2/PrintName.java
@@ -2,15 +2,23 @@ public class PrintName{
     static{
         System.loadLibrary("name");
     }
-    //the function is defined in a c-file
-    private static native void printName(final String name);
 
+    //functions are defined in a c-file
+    private static native void printName(final String name);
     private static native void greeting();
+    private static native int add(int num1, int num2);
+    private static native String append(final String str1, final String str2);
 
     public static void main(final String ... args){
 
-        printName("Jeetendra Prasad");
+        printName("[JAVA]Jeetendra Prasad");
 
         greeting();
+
+        int sum = add(23,45);
+
+        System.out.print("[JAVA]" +sum+"\n");
+
+        System.out.print("[JAVA] Printing combined message in Java. " + append("[JAVA]Message1"," [JAVA]Message2") + "\n");
     }
 }
